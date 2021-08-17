67d26b7bcbab55ceb86d4af8b7c327280622d1a5
e-nota <tat.novak@gmail.com>
2021-08-17 19:27:32 +0300

    17.08.2021

--- CopyFiles/CopyFiles/Form1.cs
+++ CopyFiles/CopyFiles/Form1.cs
@@ -13,20 +13,13 @@ namespace CopyFiles
 {
     public partial class Form1 : Form
     {
-        public string copyFile;
+        public string copyFiles;
         public Form1()
         {
             InitializeComponent();
 
-            string path1 = Path.Combine("d:", "Dir1");
-            string path2 = Path.Combine("d:", "Dir2");
-
-            string[] files1 = Directory.GetFiles("d:\\Dir1\\"); 
-            lbCopyFrom.Items.AddRange(files1);
-
-            string[] files2 = Directory.GetFiles("d:\\Dir2\\");
-            lbCopyFrom.Items.AddRange(files2);
-
+             CreateListBoxes();
+            
         }
 
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
@@ -36,26 +29,65 @@ namespace CopyFiles
 
         private void btCopy_Click(object sender, EventArgs e)
         {
-            string fileName = (string)lbCopyFrom.SelectedItem;
-            string path = fileName;
+            FileInfo file = (FileInfo)lbCopyFrom.SelectedItem;
+
+            string path = "d:\\Dir1\\";
             string pathTo = "d:\\Dir2\\" ;
 
-            copyFile = path + " -> " + "pathTo";
-            
-            Form2 formCopy = new Form2();
-            // formCopy.label1.Text = copyFile; 
-             formCopy.Show();
-
-            FileInfo file = new FileInfo(path);
-            if(file.Exists)
+            try
             {
-                file.CopyTo(pathTo, true);    
+                if (file.Exists)
+                { 
+                
+                    string fileName = file.Name;
+                    copyFiles += path + fileName + " -> " + pathTo + fileName + "\n";
+                    DataClass.Text = copyFiles;
+                  
+                    Form2 formCopy = new Form2();
+                    formCopy.Owner = this;
+                    formCopy.Show();
+
+                    file.CopyTo(pathTo + fileName, true);
+
+                  //  formCopy.Close();
+                }
+
+                
+                CreateListBoxes();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
 
-            formCopy.Close();
+        }
 
+        public void CreateListBoxes()
+        {
+            string path1 = "d:\\Dir1\\"; 
+            string path2 = "d:\\Dir2\\"; 
 
+            lbCopyFrom.Items.Clear();
+            lbCopyTo.Items.Clear();
 
+            DirectoryInfo dirInfo1 = new DirectoryInfo(path1);
+            foreach (FileInfo file1 in dirInfo1.GetFiles("*.*"))
+            {
+                //File.Copy(file.FullName, path1 + "\\" + file.Name, true);
+                lbCopyFrom.Items.Add(file1);
+            }
+
+            DirectoryInfo dirInfo2 = new DirectoryInfo(path2);
+            foreach (FileInfo file2 in dirInfo2.GetFiles("*.*"))
+            {
+                //File.Copy(file.FullName, path1 + "\\" + file.Name, true);
+                lbCopyTo.Items.Add(file2);
+            }
+
+            // string[] files1 = Directory.GetFiles("d:\\Dir1\\"); 
+            // lbCopyFrom.Items.AddRange(files1);
+            // string[] files2 = Directory.GetFiles("d:\\Dir2\\");
+            // lbCopyFrom.Items.AddRange(files2);
         }
     }
 }
--- CopyFiles/CopyFiles/Form2.cs
+++ CopyFiles/CopyFiles/Form2.cs
@@ -7,16 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static CopyFiles.Form1;
 
 namespace CopyFiles
 {
     public partial class Form2 : Form
     {
+        //public string str = "_1";
         public Form2()
         {
             InitializeComponent();
 
-           
+            label1.Text = DataClass.Text;
         }
     }
 }
